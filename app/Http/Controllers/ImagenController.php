d78189d371767b4b38e9791d0379798a7c1a2e1d
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-04-04 19:03:06 -0600

    Agregar validaciones dropzone

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Imagen;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ImagenController extends Controller
 {
@@ -28,7 +29,27 @@ class ImagenController extends Controller
      */
     public function store(Request $request)
     {
+        
+        if($request->imageable_type)
+        {
+            $tabla = '';
+            switch($request->imageable_type)
+            {
+                case 'App\Models\Lugar':
+                    $tabla = 'lugares';
+                break;
 
+                default:
+                    throw new Exception('La tabla no existe');
+                break;
+
+            }
+            
+            $request->validate([
+                'imageable_id' => ['required', 'exists:'.$tabla.',id',],
+                'imageable_type' => ['required', Rule::in(['App\Models\Lugar'])],
+            ]);
+        
             $route = $request->file->store('public');
             $pictures = new Imagen();
             $pictures->hash = $route;
@@ -38,6 +59,7 @@ class ImagenController extends Controller
             $pictures->imageable_id = $request->imageable_id;
             $pictures->imageable_type = $request->imageable_type;
             $pictures->save();
+        }
     }
 
     /**

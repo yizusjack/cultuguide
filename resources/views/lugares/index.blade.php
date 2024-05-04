aaea24ccd21bf305bd77cb9de075273b652e1a57
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-05-04 11:28:59 -0600

    Cambiar titulo

--- resources/views/lugares/index.blade.php
+++ resources/views/lugares/index.blade.php
@@ -1,42 +1,8 @@
 <x-layout
     name="Lugares"
 >
-    <h1>Suena la alarma</h1>
-
+    <h1>Lugares</h1>
     
     <livewire:listados.lugares-list/>
-
-    {{--<div class="row">
-        <div class="col-md-4 cuadro">
-          <div class="h-1">
-            <a href="/w3images/lights.jpg">
-              <img src="{{ asset('assets/imgs/artes.jpg') }}" class="image-event" alt="Lights">
-              <div class="img-thumbnail" >
-                <p>Lorem ipsum...</p>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="thumbnail">
-            <a href="/w3images/nature.jpg">
-                <img src="{{ asset('assets/imgs/icon.png') }}" alt="Lights" class="img-fluid">
-              <div class="caption">
-                <p>Lorem ipsum...</p>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="thumbnail">
-            <a href="/w3images/fjords.jpg">
-                <img src="{{ asset('assets/imgs/icon.png') }}" alt="Lights" class="img-fluid">
-              <div class="caption">
-                <p>Lorem ipsum...</p>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>--}}
 
 </x-layout>

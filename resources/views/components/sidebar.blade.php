3e05685009e432dfac9962db38146cf653fc76cb
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-05-03 21:14:03 -0600

    ya quedo la fucking interfaz

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,28 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{route('lugar.index')}}">
+            <i class="fa-solid fa-building-columns"></i>
+          <span>Lugares</span>
+        </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{route('evento.index')}}">
+            <i class="fa-regular fa-calendar"></i>
+          <span>Eventos</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{route('exhibicion.index')}}">
+            <i class="fa-solid fa-palette"></i>
+          <span>Exhibiciones</span>
+        </a>
+      </li>
+
+    </ul>
+
+  </aside>
